5969f6b3d55a5b015c74d067e390a85d0621a8cf
venomcodes0943 <dhcodes0943@gmail.com>
2023-08-23 21:57:38 +0500

    Finall work with my first class With the name of First

--- check-out.php
+++ check-out.php
@@ -1,7 +1,31 @@
 <?php
 class First
 {
-    public $num;
-    
-
+    public $num1 ,$num2, $sum;
+    function call_sum(){
+        $this->sum = $this->num1 + $this->num2;
+        return $this->sum;
+    }
+    function call_sub(){
+        $this->sum = $this->num1 - $this->num2;
+        return $this->sum;
+    }
 }
+
+$obj = new First();
+
+$obj->num1 = 12;
+$obj->num2 = 2;
+
+echo $obj->call_sub()."\n";
+
+$obj2 = new First();
+
+$obj2->num1 = 10;
+$obj2->num2 = 40;
+
+echo $obj2->call_sum()."\n";
+
+echo "Congratulations :)";
+
+
