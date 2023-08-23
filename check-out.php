a380398623a68e2334728e695c1b3214cbb7e971
venomcodes0943 <dhcodes0943@gmail.com>
2023-08-23 22:29:49 +0500

    Adding Greed To my Class With Constructor Function

--- check-out.php
+++ check-out.php
@@ -1,14 +1,30 @@
 <?php
 class First
 {
-    public $num1 ,$num2, $sum;
-    function call_sum(){
+    public $num1, $num2, $sum;
+    function call_sum()
+    {
         $this->sum = $this->num1 + $this->num2;
         return $this->sum;
     }
-    function call_sub(){
+    function call_sub()
+    {
         $this->sum = $this->num1 - $this->num2;
         return $this->sum;
+    }
+}
+class Cf
+{
+    public $name, $age, $gender;
+
+    function __construct($c_name, $c_age, $c_gender)
+    {
+        $this->name = $c_name;
+        $this->age = $c_age;
+        $this->gender = $c_gender;
+    }
+    function greed(){
+        echo $this->name . "\n". $this->age ."\n" . $this->gender ."\n" ;
     }
 }
 
@@ -17,15 +33,17 @@ $obj = new First();
 $obj->num1 = 12;
 $obj->num2 = 2;
 
-echo $obj->call_sub()."\n";
+echo $obj->call_sub() . "\n";
 
 $obj2 = new First();
 
 $obj2->num1 = 10;
 $obj2->num2 = 40;
 
-echo $obj2->call_sum()."\n";
+echo $obj2->call_sum() . "\n";
 
-echo "Congratulations :)";
+echo "Congratulations :) \n";
+echo "-------\n";
 
-
+$intro = new Cf("HASEEB",17,'M');
+$intro->greed();
