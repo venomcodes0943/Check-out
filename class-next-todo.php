d8c10c7482fe179bcf3e1c3e1a64c4a81660c388
venomcodes0943 <dhcodes0943@gmail.com>
2023-08-24 18:49:20 +0500

    Inheritance Done In Both Pages

--- class-next-todo.php
+++ class-next-todo.php
@@ -3,18 +3,53 @@
 class Todo
 {
 
-    public $car, $brand, $price;
+    public $car;
+    public $brand;
+    public $price;
 
-    function __construct($car, $brand, $price)
+    function __construct($c, $b, $p)
     {
-        $this->car = $car;
-        $this->brand = $brand;
-        $this->price = $price;
+        $this->car = $c;
+        $this->brand = $b;
+        $this->price = $p;
     }
-    // function head(){
-    //     echo "<h3>Show My Car</h3>";
-    // }
+    function head(){
+        echo "Car: " . $this->car . "\n";
+        echo "Brand: " . $this->brand . "\n";
+        echo "Price: $" . $this->price . "\n";
+    }
+    function Hi(){
+        echo "Hi i'm From Todo Class";
+    }
 }
 
-$obj = new Todo("S-class", "Marcides", 90, 0000);
-// $obj->head();
+$obj = new Todo('C-class','lamo',34000);
+$obj->head();
+// Just Change The Class Name...
+
+
+class Carmaterial extends Todo
+{
+    public $color, $shape, $size;
+
+    function __construct($Cc,$Cs, $Cz)
+    {
+        $this->color = $Cc;
+        $this->shape = $Cs;
+        $this->size = $Cz;
+    }
+
+    function displayInfo()
+    {
+        echo "Color: " . $this->color . "\n";
+        echo "Brand: " . $this->shape . "\n";
+        echo "Price: $" . $this->size . "\n";
+    }
+}
+
+$j = new Carmaterial("S-class", "Mercedes", 90000);
+echo "--------\n";
+$j->displayInfo();
+echo "--------\n";
+$j->Hi();
+?>
--- classintro.php
+++ classintro.php
@@ -13,7 +13,7 @@ class First
         return $this->sum;
     }
 }
-class Cf
+class Cf extends First
 {
     public $name, $age, $gender;
 
@@ -27,23 +27,27 @@ class Cf
         echo $this->name . "\n". $this->age ."\n" . $this->gender ."\n" ;
     }
 }
-
+// Class First Subtraction
 $obj = new First();
-
 $obj->num1 = 12;
 $obj->num2 = 2;
-
 echo $obj->call_sub() . "\n";
 
+// Class First Addition
 $obj2 = new First();
-
 $obj2->num1 = 10;
 $obj2->num2 = 40;
-
 echo $obj2->call_sum() . "\n";
+
 
 echo "Congratulations :) \n";
 echo "-------\n";
-
+// Construction Example With class Name Cf
 $intro = new Cf("HASEEB",17,'M');
-$intro->greed();
+$intro->greed() . "\n";
+echo "--------\n";
+//Inheritance Example Where Base_class is 'First' And Derived_class is 'Cf'
+$Ob = new Cf();
+$Ob->num1 = 23;
+$Ob->num2 = 13;
+echo $Ob->call_sum();
