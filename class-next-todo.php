d612ab02862f646a0d36368d97ce4b9b03adf828
venomcodes0943 <dhcodes0943@gmail.com>
2023-08-24 17:41:53 +0500

    Add inheritance in my second class with class name 'Todo' class

--- class-next-todo.php
+++ class-next-todo.php
@@ -1,6 +1,16 @@
-<?php 
-    class Todo{
-        //Inheritance
-
+<?php
+//Inheritance
+class Todo
+{
+    public $car, $brand, $price;
+    function __construct($car = "null", $brand = "null",$price = "null")
+    {
+        $this->car = $car;
+        $this->brand = $brand;
+        $this->price = $price;
+        
     }
-?>
+}
+
+$obj = new Todo();
+
